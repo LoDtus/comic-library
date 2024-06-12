d4d3c195e095df433e726c6d6f4adb93ba0a18ee
Long Nguyen <nguyentrunglong.150903@gmail.com>
2024-06-12 10:51:37 +0700

    Feat: Client - Add basic Navigation Bar 1st

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import NavigationBar from './components/NavigationBar';
 
 import Home from './components/Home';
+import ComicDetail from './components/ComicDetail';
 import Chapter from './components/Chapter';
+import Result from './components/Result';
+import Author from './components/Author';
+import User from './components/User';
 
 function App() {
     return (
@@ -13,8 +18,26 @@ function App() {
             <main className="flex justify-center min-h-[85vh] mb-5">
                 <div className="container-main lg:flex 2xl:px-3">
                     <NavigationBar/>
-                    <Home/>
-                    {/* <Chapter/> */}
+                    <Routes>
+                        <Route path='/' element={
+                            <Home/>
+                        }/>
+                        <Route path='/comic-detail' element={
+                            <ComicDetail/>
+                        }/>
+                        <Route path='/chapter' element={
+                            <Chapter/>
+                        }/>
+                        <Route path='/result' element={
+                            <Result/>
+                        }/>
+                        <Route path='/author' element={
+                            <Author/>
+                        }/>
+                        <Route path='/user' element={
+                            <User/>
+                        }/>
+                    </Routes>
                 </div>
             </main>
             <Footer/>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,31 @@
 function Home() {
-    return (
-        <div className="home">
+    const listPopular = [];
+    for (let i=0; i<10; i++) {
+        listPopular.push(
+            <div>
+                <img src={i<9 ? `./assets/posters/poster0${i+1}.jpg` : `./assets/posters/poster${i+1}.jpg`} alt=""
+                    className=""
+                />
+                <div>My Hero Academia</div>
+            </div>
+        )
+    }
 
+    return (
+        <div className="home basis-[80%] px-5 pt-5 h-full bg-[#f5f9ff] border-r">
+            <div className="bg-white shadow-md rounded-xl mt-3">
+                <div className="mb-3">
+                    <img src="./assets/thumbnail.jpg" alt=""
+                        className="w-full"
+                    />
+                </div>
+                <div>
+                    <span>Thịnh hành</span>
+                    <div className="grid">
+                        {listPopular}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
